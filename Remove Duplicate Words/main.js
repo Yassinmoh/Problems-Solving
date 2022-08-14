4fc526d608d99b3b1ebbf756b289a98ffb69333f
yassin mohamed <yassin_mohamed310@outlook.com>
2022-08-14 18:08:58 +0200

    Remove Duplicate Words

---
+++ Remove Duplicate Words/main.js
@@ -0,0 +1,34 @@
+//First Solution:
+const removeDuplicateWords = (sentence) => {
+    let result = sentence.toString().split(' ');
+    let WordsList = [];
+    for (let i = 0; i < result.length; i++) {
+        if (WordsList.indexOf(result[i]) === -1) {
+            WordsList.push(result[i]);
+        }
+    }
+    return WordsList.join(" ")
+}
+console.log(removeDuplicateWords("Hello yassin yassin i'm from from Egypt Egypt Egypt"));
+
+
+//Second Solution:
+const removeDuplicateWords2 = (sentence) => {
+    let result = sentence.toString().split(' ').filter(function (item, pos, self) {
+        return self.indexOf(item) == pos;
+    }).join(" ");
+    return result
+}
+console.log(removeDuplicateWords2("Hello yassin yassin i'm from from Egypt Egypt Egypt"));
+
+
+//Third Solution:
+const removeDuplicateWords3 = (sentence) => {
+    let result = sentence.toString().split(' ');
+    let wordInSet = new Set(result);
+    let setToArray = [...wordInSet]
+    return setToArray.join(" ")
+    //shortahnd way:
+    // return [...new Set(sentence.split(' '))].join(" ")
+}
+console.log(removeDuplicateWords3("Hello yassin yassin i'm from from Egypt Egypt Egypt"));
