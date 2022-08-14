e6db70a9be83a38dfd06bcf021df5c57ffda137b
yassin mohamed <yassin_mohamed310@outlook.com>
2022-08-14 15:06:51 +0200

    second problem

---
+++ Number To Reversed Array/main.js
@@ -0,0 +1,24 @@
+//First Selution:
+
+const convert = (num) => {
+    return num.toString().split("").map(ele => Number(ele)).reverse();
+}
+
+console.log(convert(123456789)); //[9, 8, 7, 6, 5, 4, 3, 2, 1]
+console.log(convert(56789));// [9, 8, 7, 6, 5]
+
+//Second Selution:
+
+const convertToArray = (num) => {
+    let array = [];
+    let myStr = num.toString();
+    for (let i = 0; i < myStr.length; i++) {
+        array.unshift(+myStr[i])
+    }
+    return array;
+}
+
+
+
+console.log(convertToArray(123456789)); //[9, 8, 7, 6, 5, 4, 3, 2, 1]
+console.log(convertToArray(56789));// [9, 8, 7, 6, 5]
