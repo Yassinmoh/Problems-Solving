959090141a2c39930181dff7cc014170f8127649
yassin mohamed <yassin_mohamed310@outlook.com>
2022-08-15 17:24:48 +0200

    Even or Odd

---
+++ Even or Odd/main.js
@@ -0,0 +1,9 @@
+const evenOrOdd=(num)=>{
+    if(num % 2 ==0){
+        return `Even Num`
+    }else{
+        return `Odd Num`
+    }
+}
+console.log(evenOrOdd(2)); //Even Num
+console.log(evenOrOdd(5)); //Odd Num
